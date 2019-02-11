0073e96d9587612fa53d2fb7e1dfa4f19dc34f37
Carlos Jorge Cary Huanca <gary7412@hotmail.com>
2019-02-11 13:23:02 -0400

    all project

---
+++ test/FizzBuzz_test.js
@@ -0,0 +1,14 @@
+var assert = require('assert');
+var expect = require('chai').expect;
+var should = require('chai').should();
+
+import { Fizzbuzz } from '../fizzbuzz.js';
+
+describe('Cadena Fizz.', function(){
+
+    it('Deberia devolver Fizz', function(){
+        // const fizzbuzz = new Fizzbuzz(3);
+        // expect(fizzbuzz().equal("Fizz"));
+    });
+    
+});
